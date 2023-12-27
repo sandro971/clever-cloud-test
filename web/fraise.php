add5b431431d008c064ad78d6dc78d065222f92c
sandro daninthe <sandrodaninthe@gmail.com>
2023-12-27 16:52:45 +0100

    fix sql query

--- web/fraise.php
+++ web/fraise.php
@@ -2,7 +2,7 @@
 
 require_once("peche.php");
 
-$req = $db->query('SELECT * FROM tp WHERE id == 1');
+$req = $db->query('SELECT * FROM tp WHERE id = 1');
 $data = $req->fetch();
 
 echo $data['description'];
