fd16eb5bbe16dd6e15609ac04653eceb083e7f2d
sandro daninthe <sandrodaninthe@gmail.com>
2023-12-27 16:28:07 +0100

    add port envv

--- web/peche.php
+++ web/peche.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host='.getenv("MYSQL_ADDON_HOST").';port=3306;dbname='.getenv("MYSQL_ADDON_DB"), getenv("MYSQL_ADDON_USER"), getenv("MYSQL_ADDON_PASSWORD"),
+    $db = new PDO('mysql:host='.getenv("MYSQL_ADDON_HOST").';port='.getenv("MYSQL_ADDON_PORT").';dbname='.getenv("MYSQL_ADDON_DB"), getenv("MYSQL_ADDON_USER"), getenv("MYSQL_ADDON_PASSWORD"),
     array(
         PDO::ATTR_TIMEOUT => 10, // in seconds
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
